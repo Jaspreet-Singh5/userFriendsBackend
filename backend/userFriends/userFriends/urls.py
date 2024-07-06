bcc4e4c91ba0c0eb14ed99f0304e803cc3bc8690
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2024-07-07 02:40:37 +0530

    update url file for docker old django image

--- backend/userFriends/userFriends/urls.py
+++ backend/userFriends/userFriends/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from user.views import UserViewSet, FriendViewSet, UserFriendsList
 
@@ -8,7 +8,7 @@ router.register(r'users', UserViewSet)
 router.register(r'friends', FriendViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    path('api/users/<int:user_id>/friends/', UserFriendsList.as_view(), name='user-friends'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
+    url(r'^api/users/(?P<user_id>\d+)/friends/$', UserFriendsList.as_view(), name='user-friends'),
 ]
